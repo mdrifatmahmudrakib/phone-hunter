1f9cdc29cf7ed57874667397f2c15d784bbde8c9
Rifat Mahmud <mdrifatmahmudrakib@gmail.com>
2022-03-01 05:21:49 +0600

    Error message for search and empty input

--- main.js
+++ main.js
@@ -2,21 +2,36 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = "";
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    if (searchText == '') {
+        alert('PLease Enter phone Name')
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data))
+    }
+
+
 };
 
 const displayPhone = phones => {
     console.log(phones);
     const searchResult = document.getElementById('search-result');
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
+    searchResult.textContent = '';
 
-        div.innerHTML = `
+    const errorMsg = document.getElementById('error-result');
+    if (phones.length == 0) {
+        errorMsg.style.display = 'block';
+    }
+    else {
+        errorMsg.style.display = 'none';
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+
+            div.innerHTML = `
         <div class="card h-100">
             <img src="${phone.image}" class="card-img-top w-50" alt="...">
                 <div class="card-body">
@@ -25,8 +40,10 @@ const displayPhone = phones => {
             <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" type="button">View Details</button>
                 </div>
         </div>`;
-        searchResult.appendChild(div);
-    })
+            searchResult.appendChild(div);
+        })
+    }
+
 
 }
 
@@ -42,6 +59,8 @@ const displayPhoneDetails = phone => {
     console.log(phone);
 
     const phoneDetails = document.getElementById('phone-details');
+
+    phoneDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('col');
 
@@ -57,10 +76,11 @@ const displayPhoneDetails = phone => {
         <h5 class="card-title"><strong>Memory:</strong> ${phone.mainFeatures.memory}</h5>
         <h5 class="card-title"><strong>Storage:</strong> ${phone.mainFeatures.storage}</h5>
         <h5 class="card-title"><strong>Sensors:</strong> ${phone.mainFeatures.sensors}</h5>
-        <h5class="card-title"><strong>ReleaseDate:</strong> ${phone.releaseDate ? phone.releaseDate : "Not Yet Lunched "}</h5>
+
+        <h5 class="card-title"><strong>others:</strong> ${phone.others ? phone.others : "not found"}</h5>
+        <h5 class="card-title"><strong>ReleaseDate:</strong> ${phone.releaseDate ? phone.releaseDate : "Not Yet Lunched "}</h5>
 
 
-        
         </div>
     </div>`;
     phoneDetails.appendChild(div);
